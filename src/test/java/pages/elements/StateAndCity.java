a494f845cbf5bf2cb905098ba6363885495db8d4
Михаил <mixail.anishin@mail.ru>
2025-05-14 11:51:55 +0300

    commit 4

--- src/test/java/pages/elements/StateAndCity.java
+++ src/test/java/pages/elements/StateAndCity.java
@@ -9,16 +9,24 @@ public class StateAndCity {
 
     private SelenideElement selectorState;
     private SelenideElement selectorCity;
+    private SelenideElement selectFromListState;
+    private SelenideElement selectFromListCity;
+
  
 
-    public StateAndCity(SelenideElement selectorState, SelenideElement selectorCity){
+    public StateAndCity(SelenideElement selectorState,
+                        SelenideElement selectFromListState,
+                        SelenideElement selectorCity,
+                        SelenideElement selectFromListCity){
         this.selectorState = selectorState;
+        this.selectFromListState = selectFromListState;
         this.selectorCity = selectorCity;
+        this.selectFromListCity = selectFromListCity;
 
     }
 
     @Step("кликаем по списку state")
-    public StateAndCity selectState(){
+    public StateAndCity clickState(){
         selectorState.shouldBe(Condition.visible).click();
         try {
             Thread.sleep(500);
@@ -27,9 +35,15 @@ public class StateAndCity {
         }
         return this;
     }
+
+    @Step("Выбираем из списка state")
+    public StateAndCity selectState(){
+            selectFromListState.click();
+            return this;
+    }
     
     @Step("кликаем по списку city")
-    public StateAndCity selectCity(){
+    public StateAndCity clicktCity(){
         selectorCity.shouldBe(Condition.visible).click();
         try {
             Thread.sleep(500);
@@ -37,7 +51,12 @@ public class StateAndCity {
             e.printStackTrace();
         }
         return this;
-    } 
+    }
 
+    @Step("Выбираем из списка city")
+    public StateAndCity selectCity(){
+            selectFromListCity.click();
+            return this;
+    }
 
 }
